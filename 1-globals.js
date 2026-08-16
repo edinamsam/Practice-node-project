a85387eca9a66687027598c0f5acba56966dbc4e
Samuel Adzoho <edinamsam4@gmail.com>
2026-08-16 12:57:33 +0000

    my first commit

---
+++ 1-globals.js
@@ -0,0 +1,6 @@
+const names = require("./2-names");
+const sayHi = require("./3-utils");
+require("./4-mindgrenade");
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
---
+++ 4-mindgrenade.js
@@ -0,0 +1,8 @@
+const num1 = 5;
+const num2 = 18;
+
+const sum = () => {
+  console.log(`the sum is: ${num1 + num2}`);
+};
+
+sum();
